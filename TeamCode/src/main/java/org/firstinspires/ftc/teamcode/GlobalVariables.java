26524226cf885914fe6506c5a856c5d1e4e33385
carsTEN <carstenmills@gmail.com>
2026-07-21 20:34:56 -0400

    Basic code base for command based java gradles installed + more

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GlobalVariables.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GlobalVariables.java
@@ -5,5 +5,5 @@ public class GlobalVariables {
      public static double m_autoEndHeading = 0;
      public static double m_distToTag = 0;
      public static boolean m_red = true;
-     public static boolean m_far = false;
+     public static boolean m_far = false;//
 }
